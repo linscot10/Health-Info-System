d0c326f80fcf48a5efeecfee1a61f854462fb63f
linscot10 <linscot10@gmail.com>
2025-04-25 16:24:48 +0300

    project set up

---
+++ Server/server.js
@@ -0,0 +1,20 @@
+const connectDB = require("./database/db")
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+// app.use(cors())
+
+
+
+PORT = process.env.PORT || 4000
+// database connection
+
+connectDB()
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server started in ${process.env.DEV_MODE}mode on port http://localhost:${PORT}`)
+})
